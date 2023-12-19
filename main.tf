2347bd4420d61109cbd896d9923f9cc4e6cf8462
Juliano Sathler <jsathler@live.com>
2023-12-19 07:28:33 -0300

    Changed route name in for_each to allow routes with same name in different udrs

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "azurerm_route_table" "default" {
 
 resource "azurerm_route" "default" {
   for_each               = { for key, value in local.routes : "${value.route_table_name}-${value.route_name}" => value }
-  name                   = "${each.key}-rt"
+  name                   = "${each.value.route_name}-rt"
   resource_group_name    = var.resource_group_name
   route_table_name       = azurerm_route_table.default[each.value.route_table_name].name
   address_prefix         = each.value.address_prefix
